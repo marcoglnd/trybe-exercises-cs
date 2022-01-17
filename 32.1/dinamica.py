598e57d810858be0e5a9bc174bed77cd296e2c41
Marco Galindo <marco1408@gmail.com>
2022-01-17 17:52:28 -0300

    terminando exercicios de fixacao e comecando exercicios do dia

---
+++ 32.1/dinamica.py
@@ -0,0 +1,12 @@
+# coding=UTF-8
+
+lista_frutas = ['banana', 'uva', 'maçã', 'pera']
+
+# for fruta in lista_frutas:
+#     print(fruta[0])
+
+# for fruta in lista_frutas:
+#     print(fruta[3])
+
+tupla_frutas = tuple(lista_frutas)
+print(tupla_frutas)
