1f221989b411c0530e968df5ed99acf2407c147d
Marco Galindo <marco1408@gmail.com>
2022-01-18 11:41:13 -0300

    adicionando exercicio 1

---
+++ 32.2/python-io/fixacao.py
@@ -0,0 +1,15 @@
+# Exercício 1: Faça um programa que solicite o nome de uma pessoa usuária e imprima-o na vertical. Exemplo:
+
+name = input("Digite seu nome: ")
+for letter in name:
+    print(f"{letter}")
+
+# Exercício 2: Escreva um programa que receba vários números naturais e calcule a soma desses valores.
+# Caso algum valor da entrada seja inválido, por exemplo uma letra, uma mensagem deve ser exibida,
+# na saída de erros, no seguinte formato: "Erro ao somar valores, {valor} é um valor inválido".
+# Ao final, você deve imprimir a soma dos valores válidos.
+
+#Receba os valores em um mesmo input , solicitando à pessoa usuária que separe-os com um espaço.
+# Receba os valores no formato str e utilize o método split para pegar cada valor separado.
+# O método isdigit , embutido no tipo str , pode ser utilizado para verificar se a string corresponde a um número natural.
+
